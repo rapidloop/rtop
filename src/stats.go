a072fe21ad41c1c73e1dedea50b3cf540126fb8c
Mahadevan <mahadevan@rapidloop.com>
2015-05-20 11:23:40 +0530

    Support for LVM partitions on CentOS 6. Closes #15.

--- src/stats.go
+++ src/stats.go
@@ -168,20 +168,27 @@ func getFSInfo(client *ssh.Client, stats *Stats) (err error) {
 	}
 
 	scanner := bufio.NewScanner(strings.NewReader(lines))
+	flag := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Fields(line)
-		if len(parts) == 6 && strings.Index(parts[0], "/dev/") == 0 {
-			used, err := strconv.ParseUint(parts[2], 10, 64)
+		n := len(parts)
+		dev := n > 0 && strings.Index(parts[0], "/dev/") == 0
+		if n == 1 && dev {
+			flag = 1
+		} else if (n == 5 && flag == 1) || (n == 6 && dev) {
+			i := flag
+			flag = 0
+			used, err := strconv.ParseUint(parts[2-i], 10, 64)
 			if err != nil {
 				continue
 			}
-			free, err := strconv.ParseUint(parts[3], 10, 64)
+			free, err := strconv.ParseUint(parts[3-i], 10, 64)
 			if err != nil {
 				continue
 			}
 			stats.FSInfos = append(stats.FSInfos, FSInfo{
-				parts[5], used, free,
+				parts[5-i], used, free,
 			})
 		}
 	}
