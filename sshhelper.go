94c21b67f52db5c53d31a9e4233739a5770e1c59
Machiel Molenaar <machiel@machiel.me>
2015-09-28 15:22:55 +0200

    Expand user home directory

--- sshhelper.go
+++ sshhelper.go
@@ -36,6 +36,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"os/user"
 	"strings"
 	"syscall"
 
@@ -98,10 +99,31 @@ func ParsePemBlock(block *pem.Block) (interface{}, error) {
 	}
 }
 
+func expandPath(path string) string {
+
+	if len(path) < 2 {
+		return path
+	}
+
+	if path[:2] != "~/" {
+		return path
+	}
+
+	usr, err := user.Current()
+
+	if err != nil {
+		return path
+	}
+
+	return strings.Replace(path, "~", usr.HomeDir, 1)
+}
+
 func addKeyAuth(auths []ssh.AuthMethod, keypath string) []ssh.AuthMethod {
 	if len(keypath) == 0 {
 		return auths
 	}
+
+	keypath = expandPath(keypath)
 
 	// read the file
 	pemBytes, err := ioutil.ReadFile(keypath)
